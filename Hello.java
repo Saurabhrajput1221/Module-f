a7e7551af29a4647f9935ff6812d42801de01631
saurabh rajput <saurabhkgf2609@gmail.com>
2022-08-04 14:02:24 +0530

    intialize commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    
+    public static void main(String[] args) {
+        System.out.println("hello.....");
+        for(int i =0;i<100;i++){
+            System.out.print(i + "  ");
+        }
+    }
+}
